5083955459e2da4a8a5ddce7263de4b96c409213
Maria De La Osa Martinez <mdelaosa@gamma18.aulas.gsyc.urjc.es>
2018-12-10 09:31:21 +0100

    Pruebas

--- uaclient.py
+++ uaclient.py
@@ -3,40 +3,48 @@
 """Cliente UA práctica final María de la Osa."""
 
 import sys
+import socket
 import time
 import os
 
 
 def log(self, fichlog):
     with open(fichlog, 'w') as log:
-        fichlog.dump(self.data, log, indent=3)
+        json.dump(self.data, log, indent=3)
+
 
 try:
-    config = sys.argv[1]  # Fichero XML.
-    method = sys.argv[2]  # Método SIP.
-    option = sys.argv[3]  # Parámetro opcional.
+    CONFIG = sys.argv[1]  # Fichero XML.
+    METHOD = sys.argv[2]  # Método SIP.
+    OPTION = sys.argv[3]  # Parámetro opcional.
 
-    file = open(config, 'r')
+    file = open(CONFIG, 'r')
     line = file.readlines()
 
+    USERNAME = str(line[4].split('="')[1].split('"'))
+    IP = str(line[5].split('="')[1].split('"'))
+    PORT = str(line[5].split('="')[2].split('"'))
+    print('USER:' + USERNAME + 'IP:' + IP + 'PORT:' + PORT)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((server, PORT))
+
+        if METHOD == 'REGISTER':
+            #password = line[4].split('="')[2].split('"')
+            USER = ('REGISTER sip:' + USERNAME + ':' + PORT + 'SIP/2.0\r\n' + 'Expires:' + OPTION + '\r\n')
+            print(USER)
+            '''if  :
+                print('SIP/2.0 401 Unaunthorized')
+                print('WWW Authenticate: Digest nonce="898989898798989898989')
+            else:
+                print(user + 'Authorizarion:Digest response="123123212312321212123')
+    
+        if METHOD == 'INVITE':
+    
+        if METHOD == 'BYE':
+    '''
     file.close()
-
-    if method == 'REGISTER':
-        username = str(line[4].split('="')[1].split('"'))
-        port = str(line[5].split('="')[2].split('"'))
-        #password = line[4].split('="')[2].split('"')
-        user = ('REGISTER sip:' + username + ':' + port + 'SIP/2.0\r\n' + 'Expires:' + option + '\r\n')
-        if  :
-            print('SIP/2.0 401 Unaunthorized')
-            print('WWW Authenticate: Digest nonce="898989898798989898989')
-        else:
-            print(user + 'Authorizarion:Digest response="123123212312321212123')
-
-    if method == 'INVITE':
-
-    if method == 'BYE':
-
-
 except IndexError:
     print("Usage: python3 uaclient.py config method option")
 
@@ -45,7 +53,4 @@ def datetime():
     time_actual_str = time.strftime('%Y-%m-%d %H:%M:%S',
                                     time.gmtime(time_actual))
 
-
-
-if __name__ == "__main__":
 
