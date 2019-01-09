ce0d17b82c8ba8ac3aabad9a32612caaa6f95af5
Maria De La Osa Martinez <mdelaosa@alpha13.aulas.gsyc.urjc.es>
2019-01-09 20:22:41 +0100

    Pep8 & Pep257 Proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -12,7 +12,6 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-
 class Logging:
     """Creamos la clase del Log."""
 
@@ -23,43 +22,49 @@ class Logging:
         logfile.write(time_actual + ' ' + str(operacion))
         logfile.close()
 
+
 class ProxyHandleXML(ContentHandler):
+    """Creamos la clase del documento XML."""
 
     def __init__(self):
-        self.dictags = {'server': ['name', 'ip', 'puerto'], 'database': ['path',
-                        'passwdpath'], 'log': ['path']}
+        """Función principal."""
+        self.dictags = {'server': ['name', 'ip', 'puerto'],
+                        'database': ['path', 'passwdpath'],
+                        'log': ['path']}
         self.dproxy = {}
 
     def startElement(self, tag, attrs):
+        """Función atributos."""
         if tag in self.dictags.keys():
             for parameters in self.dictags[tag]:
                 self.dproxy[tag + '_' + parameters] = attrs.get(parameters, '')
 
     def get_tags(self):
+        """Función etiquetas."""
         return self.dproxy
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """Clase Proxy"""
+    """Clase Proxy."""
 
     nonce = {}
+
     def json2register(self):
-        # Avanzado.
+        """Avanzado. Cogemos lo guardado anteriormetne."""
         try:
             with open(DATABASE, 'r') as clientsfile:
                 self.clientes = json.load(clientsfile)
         except (FileNotFoundError or ValueError):
             self.clientes = {}
 
-
     def register2json(self):
-        # Database.
+        """Database."""
         with open(DATABASE, 'w') as archivo_json:
             json.dump(self.clientes, archivo_json, sort_keys=True,
                       indent=4)
 
     def passwords(self):
-        # Contraseñas.
+        """Contraseñas."""
         try:
             with open(DATAPASS, 'r') as PASSWORDS:
                 self.passwds = json.load(PASSWORDS)
@@ -67,6 +72,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             pass
 
     def abrirsocket(self, mensaje, IPS, PORTS):
+        """Creación del socket."""
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             my_socket.connect((IPS, int(PORTS)))
@@ -81,20 +87,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         """Escribe dirección y puerto del cliente (de tupla client_address)."""
         self.json2register()
-
         self.passwords()
 
         CLIENT = self.client_address[0]
-        PORTCLIENT = self.client_address[1]
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente.
             line = self.rfile.read().decode('utf-8')
             if not line:
                 break
-            #information = line.decode('utf-8')
             info = line.split(' ')
             METHOD = info[0]
-            USER1 = info[1].split(':')[1]
+            U1 = info[1].split(':')[1]
             # Si no hay más líneas salimos del bucle.
             if not line:
                 break
@@ -108,21 +111,22 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 Logging.log('Received from' + CLIENT + ':' + C_PORT + ': '
                             + " ".join(info) + '\r\n')
                 if len(info) == 5:
-                    if USER1 in self.passwds:
-                        if USER1 in self.clientes:
+                    if U1 in self.passwds:
+                        if U1 in self.clientes:
                             # Avanzada: Refuerzo de errores.
                             if EXP == 0:
                                 # Borrar un usuario.
-                                del self.clientes[USER1]
-                                self.wfile.write(bytes('SIP/2.0 200 OK. Deleting' +
-                                                       '\r\n', 'utf-8'))
-                                Logging.log('Sent to:' + CLIENT + ':' + C_PORT +
-                                            ': SIP/2.0 200 OK. Deleting user.\r\n')
+                                del self.clientes[U1]
+                                self.wfile.write(bytes('SIP/2.0 200 OK. '
+                                                       'Deleting.\r\n',
+                                                       'utf-8'))
+                                Logging.log('Sent: ' + CLIENT + ':' + C_PORT +
+                                            ': SIP/2.0 200 OK. Deleting.\r\n')
                             if EXP < 0:
                                 self.wfile.write(bytes('400 BAD REQUEST.\r\n',
                                                        'utf-8'))
                                 print('400 BAD REQUEST.')
-                                Logging.log('Sent to:' + CLIENT + ':' + C_PORT +
+                                Logging.log('Sent: ' + CLIENT + ':' + C_PORT +
                                             ': 400 BAD REQUEST.\r\n')
                             if EXP > 0:
                                 self.wfile.write(bytes('SIP/2.0 200 OK.\r\n',
@@ -130,10 +134,11 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                                 Logging.log('Sent to' + CLIENT + ':' + C_PORT +
                                             ': SIP/2.0 200 OK.\r\n')
                         else:
-                            self.nonce[USER1] = str(random.randint(0, 999999999999999))
-                            errormens = ('FALLA AQUÍ 401 USER NOT FOUND.\r\n WWW' +
-                                         'Authenticate: Digest nonce=' + self.nonce[USER1])
-                            self.wfile.write(bytes(errormens, 'utf-8'))
+                            self.nonce[U1] = str(random.randint(0, 9999999999))
+                            error = ('401 USER NOT FOUND.\r\n WWW' +
+                                     'Authenticate: Digest nonce=' +
+                                     self.nonce[U1])
+                            self.wfile.write(bytes(error, 'utf-8'))
 
                 if len(info) == 8:
                     if EXP < 0:
@@ -145,41 +150,46 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
                     nonce_recv = info.split('=')[1].split('\r')[0]
                     checking = hashlib.md5()
-                    checking.update(bytes(self.passwds[USER1]['passwd'], 'utf-8'))
-                    checking.update(bytes(self.nonce[USER1], 'utf-8'))
+                    checking.update(bytes(self.passwds[U1]['passwd'], 'utf-8'))
+                    checking.update(bytes(self.nonce[U1], 'utf-8'))
                     if nonce_recv == checking.hexdigest() and EXP > 0:
                         self.wfile.write(bytes('SIP/2.0 200 OK. Registered.' +
                                                '\r\n', 'utf-8'))
                         Logging.log('Sent to' + CLIENT + ':' + C_PORT +
                                     ': SIP/2.0 200 OK. Registered.\r\n')
-                        self.clientes[USER1] = {'IP': CLIENT, 'PORT': C_PORT,
-                                                'TIME': TIME,
-                                                'EXPIRES': (EXP + TIME)}
+                        self.clientes[U1] = {'IP': CLIENT, 'PORT': C_PORT,
+                                             'TIME': TIME,
+                                             'EXPIRES': (EXP + TIME)}
             elif METHOD == 'INVITE':
                 print(line)
-                if USER1 in self.clientes:
-                    USER2 = info[7].split('o=')[1].split(' ')[0]
-                    print(USER2)
-                    if USER2 in self.clientes:
-                        Logging.log('Sent to' + self.clientes[USER1]['IP'] + ':' + self.clientes[USER1]['PORT'] + ':' +
+                if U1 in self.clientes:
+                    U2 = info[7].split('o=')[1].split(' ')[0]
+                    print(U2)
+                    if U2 in self.clientes:
+                        Logging.log('Sent to' + self.clientes[U1]['IP'] + ':' +
+                                    self.clientes[U1]['PORT'] + ':' +
                                     line + '\r\n')
-                        recibo = self.abrirsocket(line, self.clientes[USER1]['IP'], self.clientes[USER1]['PORT'])
-                        self.wfile.write(bytes(recibo + '\r\n', 'utf-8'))
+                        rc = self.abrirsocket(line, self.clientes[U1]['IP'],
+                                              self.clientes[U1]['PORT'])
+                        self.wfile.write(bytes(rc + '\r\n', 'utf-8'))
                         if METHOD == 'ACK':
-                            Logging.log('Sent to' + self.clientes[USER1]['IP'] + ':' + self.clientes[USER1]['PORT'] + ':' +
-                                line + '\r\n')
-                            recibo = self.abrirsocket(line, self.clientes[USER1]['IP'], self.clientes[USER1]['PORT'])
-                            self.wfile.write(bytes(recibo + '\r\n', 'utf-8'))
+                            Logging.log('Sent to' + self.clientes[U1]['IP'] +
+                                        ':' + self.clientes[U1]['PORT'] + ':' +
+                                        line + '\r\n')
+                            rc = self.abrirsocket(line,
+                                                  self.clientes[U1]['IP'],
+                                                  self.clientes[U1]['PORT'])
+                            self.wfile.write(bytes(rc + '\r\n', 'utf-8'))
                 else:
                     self.wfile.write(bytes('404 USER NOT FOUND.\r\n', 'utf-8'))
                     print('404 USER NOT FOUND.')
-                    Logging.log('Sent to:' + CLIENT + ': 404 USER NOT FOUND.\r\n')
+                    Logging.log('Sent to:' + CLIENT +
+                                ': 404 USER NOT FOUND.\r\n')
 
             elif METHOD == 'BYE':
                 C_PORT = info[1].split(':')[2].split('SIP')[0]
                 Logging.log('Sent to:' + CLIENT + ':' + C_PORT +
                             ': BYE. FINISHING CONNECTION.\r\n')
-                #del self.clientes[USER1]
 
             elif METHOD != ('REGISTER' or 'INVITE' or 'ACK' or 'BYE'):
                 self.wfile.write(b"SIP/2.0 405 METHOD NOT ALLOWED\r\n")
